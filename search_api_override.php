e1fb154770ef95f64c5ad72f7f1c3e6995bfeed4
M Parker <mparker17@users.noreply.github.com>
2015-02-10 11:40:19 -0500

    Need a leading slash in the path to a Search API Solr server.

--- search_api_override.php
+++ search_api_override.php
@@ -5,5 +5,5 @@ $conf['search_api_override_mode'] = 'load';
 $conf['search_api_override_servers']['server_machine_name']['options'] = array(
   'host' => 'hostname',
   'port' => '8080',
-  'path' => 'solr/collection1',
+  'path' => '/solr/collection1',
 );
