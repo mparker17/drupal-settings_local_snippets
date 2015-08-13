43d75b49706aac16308e3ac522e0631dc06e4c18
M Parker <mparker17@users.noreply.github.com>
2015-08-13 09:52:10 -0400

    Only use stage_file_proxy on local, testing, staging.

--- stage_file_proxy.php
+++ stage_file_proxy.php
@@ -1,5 +1,5 @@
 <?php
 
-// Stage file proxy.
+// On local, testing, and staging servers, lazy-download images from production.
 $conf['stage_file_proxy_origin'] = 'http://live.example.com';
 $conf['stage_file_proxy_use_imagecache_root'] = TRUE;
