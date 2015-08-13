53de193e3a2450f2ace1f9cd9e1cadb74d3c0108
M Parker <mparker17@users.noreply.github.com>
2015-08-13 09:39:51 -0400

    Make usage comments clearer.

--- drupal.php
+++ drupal.php
@@ -8,7 +8,7 @@ $conf['file_temporary_path'] = '/tmp';
 // URLs.
 $base_url = 'http://example.dev';
 
-// Disable caching on local and testing servers.
+// On local and testing servers, disable caching.
 $conf['cache'] = FALSE;
 $conf['block_cache'] = FALSE;
 $conf['cache_lifetime'] = 0;
@@ -16,8 +16,8 @@ $conf['page_cache_maximum_age'] = 0;
 #$conf['preprocess_css'] = 0;
 #$conf['preprocess_js'] = 0;
 
-// Always display all logging and error messages locally.
+// On local and testing servers, display all logging and error messages.
 $conf['error_level'] = 2;
 
-// Redirect e-mails to a log file.
+// On local and testing servers, redirect outgoing e-mails to a log file.
 ini_set('sendmail_path', 'tee -a /Applications/MAMP/Library/logs/example.mail.log > /dev/null');
