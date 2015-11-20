60cebcd4b578c66232f1e63466db1e04c3d2e561
M Parker <mparker17@users.noreply.github.com>
2015-11-20 10:41:16 -0500

    t() isn't guaranteed to work as early as settings.[*.]php

--- search_api_solr_overrides.php
+++ search_api_solr_overrides.php
@@ -2,7 +2,7 @@
 
 // Search API Solr Overrides module.
 $conf['search_api_solr_overrides']['server_machine_name'] = array(
-  'name' => t('Solr Server (Overridden)'),
+  'name' => 'Solr Server (Overridden)',
   'options' => array(
     'host' => 'hostname',
     'port' => 8983,
