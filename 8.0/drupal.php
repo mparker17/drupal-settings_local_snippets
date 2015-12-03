cf8568fe5e82ec2869a2b479c93dba13dc2d70f0
M Parker <mparker17@536298.no-reply.drupal.org>
2015-12-03 12:25:25 -0500

    Add trusted host patterns, comment out development services by default.

--- 8.0/drupal.php
+++ 8.0/drupal.php
@@ -3,6 +3,11 @@
 // PHP settings.
 ini_set('date.timezone', 'UTC');
 
+// URLs.
+$settings['trusted_host_patterns'] = array(
+  '^example\.dev$',
+);
+
 // Filesystem.
 $settings['file_public_path'] = 'sites/default/files';
 $settings['file_private_path'] = '/tmp';
@@ -20,7 +25,7 @@ assert_options(ASSERT_ACTIVE, TRUE);
 $config['system.logging']['error_level'] = 'verbose';
 
 // Local development services and caches.
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+#$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 #$settings['cache']['bins']['render'] = 'cache.backend.null';
 #$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 
