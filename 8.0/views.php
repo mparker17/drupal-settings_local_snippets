555bc4bab7ed6dff1957206e0ec988058b0afcf4
M Parker <mparker17@users.noreply.github.com>
2015-12-07 15:05:39 -0500

    Create views.php
    Ensures advanced views display settings are used.

---
+++ 8.0/views.php
@@ -0,0 +1,6 @@
+<?php
+
+// Views settings.
+$config['views.settings']['ui.show.master_display'] = TRUE;
+$config['views.settings']['ui.advanced_column'] = TRUE;
+$config['views.settings']['ui.show.display_embed'] = TRUE;
