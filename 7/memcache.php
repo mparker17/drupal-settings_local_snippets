9ec02632022e4eecb9a91efa1f508d9195b23e48
M Parker <mparker17@536298.no-reply.drupal.org>
2016-04-12 18:09:37 -0400

    Add additional settings from the memcache module's README.

--- 7/memcache.php
+++ 7/memcache.php
@@ -4,8 +4,12 @@
 $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
 $conf['cache_default_class'] = 'MemCacheDrupal';
 $conf['memcache_bins'] = array('cache' => 'default');
+$conf['memcache_stampede_protection'] = TRUE;
+$conf['page_cache_without_database'] = TRUE;
+$conf['page_cache_invoke_hooks'] = FALSE;
 // Path to the memcache module's 'memcache.inc' file. Default usually fine.
 $conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
+$conf['lock_inc'] = 'sites/all/modules/contrib/memcache/memcache-lock.inc';
 // Hostname:port of the memcache server. Default usually fine.
 $conf['memcache_servers'] = array('memcache:11211' => 'default');
 // CHANGEME: Set a unique prefix for this Drupal installation.
