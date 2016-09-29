98a9440e436c243b681745bf59c19a948bdf0486
M Parker <mparker17@536298.no-reply.drupal.org>
2016-09-29 00:35:48 -0400

    Add D8 settings for environment_indicator module.

---
+++ 8.x/environment_indicator.php
@@ -0,0 +1,18 @@
+<?php
+
+// Environment indicator module.
+$config['environment_indicator.settings']['git'] = TRUE;
+$config['environment_indicator.indicator']['name'] = php_uname('n');
+$config['environment_indicator.indicator']['bg_color'] = '#FF00FF';
+
+// Environment indicator module.
+$config['environment_indicator.indicator']['name'] = 'testing';
+$config['environment_indicator.indicator']['bg_color'] = '#32CD32';
+
+// Environment indicator module.
+$config['environment_indicator.indicator']['name'] = 'staging';
+$config['environment_indicator.indicator']['bg_color'] = '#ff8000';
+
+// Environment indicator module.
+$config['environment_indicator.indicator']['name'] = 'live';
+$config['environment_indicator.indicator']['bg_color'] = '#8B0000';
