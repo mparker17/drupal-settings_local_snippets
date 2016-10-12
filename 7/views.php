a47e5bc16e4b50de37a8d1158c402e174d98bb23
M Parker <mparker17@536298.no-reply.drupal.org>
2016-10-12 14:36:52 -0400

    Add ability to override theme for views UI.

--- 7/views.php
+++ 7/views.php
@@ -6,3 +6,4 @@ $conf['views_ui_show_advanced_help_warning'] = FALSE;
 $conf['views_ui_show_master_display'] = TRUE;
 $conf['views_ui_show_advanced_column'] = TRUE;
 $conf['views_ui_display_embed'] = TRUE;
+#$conf['views_ui_custom_theme'] = 'seven';
