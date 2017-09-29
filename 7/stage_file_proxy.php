eedb61309e2475777889e0cc5905eb3e7a0eb80d
M Parker <mparker17@users.noreply.github.com>
2017-09-29 10:48:12 -0400

    Modernize stage_file_proxy settings for D7.

--- 7/stage_file_proxy.php
+++ 7/stage_file_proxy.php
@@ -1,5 +1,7 @@
 <?php
 
-// On local, testing, and staging servers, lazy-download images from production.
-$conf['stage_file_proxy_origin'] = 'http://live.example.com';
-$conf['stage_file_proxy_use_imagecache_root'] = TRUE;
+// On local, testing, and staging servers, lazy-download images from a shared
+// environment.
+$conf['stage_file_proxy_origin'] = 'https://user:pass@example.com';
+$conf['stage_file_proxy_hotlink'] = TRUE;
+#$conf['stage_file_proxy_origin_dir'] = 'sites/example/files';
